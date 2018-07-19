73b1201513ca3b6e48628aa7bdd7de0042784446
JZ057578 <jonah.zukosky@cerner.com>
2018-07-19 09:55:06 -0500

    Map Added

---
+++ DataRoutr/MapViewController.swift
@@ -0,0 +1,50 @@
+//
+//  MapViewController.swift
+//  DataRoutr
+//
+//  Created by Zukosky,Jonah on 7/19/18.
+//  Copyright © 2018 Zukosky,Jonah. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager: CLLocationManager = CLLocationManager()
+    var currentLocation: CLLocation?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Map Setup
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        currentLocation = nil
+        
+        mapView.delegate = self
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
