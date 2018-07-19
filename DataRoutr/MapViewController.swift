23125b3aa78a3c9e5114a78d605a32b428c90ae9
JZ057578 <jonah.zukosky@cerner.com>
2018-07-19 10:14:16 -0500

    Added focus on user

--- DataRoutr/MapViewController.swift
+++ DataRoutr/MapViewController.swift
@@ -8,43 +8,46 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+class MapViewController: UIViewController, MKMapViewDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     
     let locationManager: CLLocationManager = CLLocationManager()
     var currentLocation: CLLocation?
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Map Setup
+        mapView.showsUserLocation = true
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
         currentLocation = nil
-        
-        mapView.delegate = self
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+extension MapViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if locations.count != 0 {
+            if currentLocation == nil {
+                if let location = locations.first {
+                    let span = MKCoordinateSpanMake(0.05, 0.05)
+                    let region = MKCoordinateRegion(center: location.coordinate, span: span)
+                    mapView.setRegion(region, animated: true)
+                }
+            }
+            currentLocation = locations[locations.count - 1]
+        }
     }
-    */
-
 }
